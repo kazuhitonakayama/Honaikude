1ef9e006ecacd7626e7c412190ad4f639ac921c7
中山一仁 <firstjin19990101@gmail.com>
2019-11-17 08:24:41 +0900

    Honaikude LP HTML 実装 80%

---
+++ js/script.js
@@ -0,0 +1,41 @@
+$(function(){
+	
+	var
+	  winW = $(window).width(),
+		winH = $(window).height(),
+		nav = $('#mainnav ul a'),
+		curPos = $(this).scrollTop();
+	
+	if (winW > 800){
+		var headerH =0;
+	}
+	else{
+		var headerH =70;
+	}
+	
+	$(nav).on('click', function(){
+  	var $el = $(this),
+		id = $el.attr('href');
+ 		$('html, body').animate({
+   		scrollTop: $(id).offset().top - headerH
+ 		}, 500);
+		if (winW < 890){
+			$('#menuWrap').next().slideToggle();
+			$('#menuBtn').removeClass('close');
+		}
+ 		return false;
+	});
+	
+	$('.panel').hide();
+	$('#menuWrap').toggle(function(){
+		$(this).next().slideToggle();
+		$('#menuBtn').toggleClass('close');
+	},
+	function(){
+		$(this).next().slideToggle();
+		$('#menuBtn').removeClass('close');
+	});
+	$('h1.ourmission').hide().fadeIn(10000);
+	$('span.large').hide().fadeIn(100);
+});
+
